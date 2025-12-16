e636d20a690182ca2c2335a3d465f65661417976
defintNull / Lorenzo + D'Angelo <lorenzo.dangelo.mail@gmail.com>
2025-12-16 16:33:12 +0100

    First push

---
+++ resources/js/App.jsx
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h1>Hello React + Laravel!</h1>;
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
