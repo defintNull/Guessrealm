42a3d33e53ed9bac29e0fd41fabc5878c09f9783
Di-Simone-dev <andrealucadisimone@gmail.com>
2026-01-05 11:08:27 +0100

    fix userfactory e aggiunta seeding utenti dev

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        //reminder, usare il comando
+        //php artisan migrate:fresh --seed
+        //droppa tutto,remigra e riesegue i seeder
 
+
+
+        //utente Andrea Luca Di Simone id = 1
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Andrea Luca',
+            'surname' => 'Di Simone',
+            'email' => 'andrealuca.disimone@student.univaq.it',
+            'email_verified_at' => now(),
+            'username' => 'andrealuca.disimone',
+            'password' => bcrypt('guessrealm'),
         ]);
+
+        //utente Lorenzo D'Angelo id = 2
+        User::factory()->create([
+            'name' => 'Lorenzo',
+            'surname' => "D'Angelo",
+            'email' => 'lorenzo.dangelo1@student.univaq.it',
+            'email_verified_at' => now(),
+            'username' => 'lorenzo.dangelo',
+            'password' => bcrypt('guessrealm'),
+        ]);
+
+        //utente Andrea iannotti  id = 3
+        User::factory()->create([
+            'name' => 'Andrea',
+            'surname' => 'Iannotti',
+            'email' => 'andrea.iannotti@student.univaq.it',
+            'email_verified_at' => now(),
+            'username' => 'andrea.iannotti',
+            'password' => bcrypt('guessrealm'),
+        ]);
+
+        //utente prof Serafino Cicerone id = 4
+        User::factory()->create([
+            'name' => 'Serafino',
+            'surname' => "Cicerone",
+            'email' => 'serafino.cicerone@univaq.it',
+            'email_verified_at' => now(),
+            'username' => 'serafino.cicerone',
+            'password' => bcrypt('frontend!'),
+        ]);
+
+        //User::factory(46)->create();
+
     }
 }
